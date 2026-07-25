11ca123c8b163fc79ee86742b36c567b6294c62e
Throsfox <throsfox@gmail.com>
2026-07-25 17:11:51 -0300

    Primeiro código para criação do DB

--- main.py
+++ main.py
@@ -2,3 +2,15 @@ import sqlite3
 
 conexao = sqlite3.connect("banco.db")
 cursor = conexao.cursor()
+
+cursor.execute("""CREATE TABLE IF NOT EXISTS usuarios (
+            id  INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+            nome TEXT NOT NULL,
+            email VARCHAR(255) NOT NULL,
+            senha TEXT NOT NULL)""")
+
+cursor.execute("""INSERT INTO usuarios
+            (nome, email, senha) VALUES
+            ('pedro', 'pedro@gmail.com', '1234')""")
+
+conexao.commit()
